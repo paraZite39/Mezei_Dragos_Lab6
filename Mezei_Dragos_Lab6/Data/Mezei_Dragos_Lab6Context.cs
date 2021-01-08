3d30f41c17ce4d81467f4cab787c214f44c125b4
paraZite <paraZite@DESKTOP-2U8TDJM>
2021-01-08 16:22:06 +0200

    Add project files.

---
+++ Mezei_Dragos_Lab6/Data/Mezei_Dragos_Lab6Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Mezei_Dragos_Lab6.Models;
+
+namespace Mezei_Dragos_Lab6.Data
+{
+    public class Mezei_Dragos_Lab6Context : DbContext
+    {
+        public Mezei_Dragos_Lab6Context (DbContextOptions<Mezei_Dragos_Lab6Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Mezei_Dragos_Lab6.Models.Book> Book { get; set; }
+    }
+}
---
+++ Mezei_Dragos_Lab6/Pages/Books/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Mezei_Dragos_Lab6.Data;
+using Mezei_Dragos_Lab6.Models;
+
+namespace Mezei_Dragos_Lab6.Pages.Books
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Mezei_Dragos_Lab6.Data.Mezei_Dragos_Lab6Context _context;
+
+        public DetailsModel(Mezei_Dragos_Lab6.Data.Mezei_Dragos_Lab6Context context)
+        {
+            _context = context;
+        }
+
+        public Book Book { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Book == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
